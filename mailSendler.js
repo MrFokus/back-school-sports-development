bff1cc0328caf98f04dafd1fb6eee01ac6238ab3
GlubokovGV <golubkovgv@admlr.lipetsk.ru>
2023-06-22 01:50:27 +0300

    feat: add new mail address

--- mailSendler.js
+++ mailSendler.js
@@ -5,8 +5,8 @@ let transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: 'german_glubokov@mail.ru',
-        pass: 'tp5RsbzRqckqWinHxEft',
+        user: 'point20000000@mail.ru',
+        pass: 'JZbX3582zmD1ty5zb5cT',
     },
 });
 
@@ -15,7 +15,7 @@ let sendFunc = async (mail,phone,comment)=>
     try{
         let result = await transporter.sendMail({
             from: '"НОВАЯ ЗАЯВКА НА ЗАПИСЬ!" <german_glubokov@mail.ru>',
-            to: 'german_glubokov@mail.ru',
+            to: 'point20000000@mail.ru',
             subject: 'Сообщение от '+mail,
             text: `Новая заявка на запись \n Телефон: ${phone}\n Комментарий: ${comment}`,
             html:
